d4712e05af4c54dd351af8e2651e83733a2d1ada
Malik <moiez.malik@ig.bz>
2022-04-18 12:18:43 -0400

    javascript file deleted

--- rss-feeder.js
+++
@@ -1,6 +0,0 @@
-const RSS_URL = `https://feeds.libsyn.com/173168/rss`;
-
-fetch(RSS_URL)
-  .then(response => response.text())
-  .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-  .then(data => console.log(data))
